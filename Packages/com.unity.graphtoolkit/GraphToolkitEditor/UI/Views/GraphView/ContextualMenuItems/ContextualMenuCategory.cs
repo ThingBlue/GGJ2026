bb137c032821f1467c4991fede27d85fee96162f
NWillsher <nawillsher@gmail.com>
2026-01-25 20:26:07 -0500

    Added Graph System Package

---
+++ Packages/com.unity.graphtoolkit/GraphToolkitEditor/UI/Views/GraphView/ContextualMenuItems/ContextualMenuCategory.cs
@@ -0,0 +1,20 @@
+﻿namespace Unity.GraphToolkit.Editor.ContextualMenuItems
+{
+    /// <summary>
+    /// The categories for contextual menu items. They appear in the menu in the order defined here.
+    /// </summary>
+    enum ContextualMenuCategory
+    {
+        FunctionalElements,
+        OrganizationalElements,
+        ToolSpecificElements,
+        Conversions,
+        CutCopyPaste,
+        RenameDuplicateDelete,
+        AssetManagement,
+        Modifications,
+        Organization,
+        Advanced,
+        External
+    }
+}
